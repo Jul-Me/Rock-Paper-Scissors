15a63577a997fa73ec626a8ead7ad3e88b2af3ad
Jul-Me <jmdev.uk@gmail.com>
2021-01-27 14:01:21 +0000

    Added house selection functionality

--- scripts.js
+++ scripts.js
@@ -5,24 +5,29 @@ const step1El = document.getElementById('step1');
 const step2El = document.getElementById('step2');
 const resultEl = document.getElementById('result');
 const playerSelectionEl = document.getElementById('playerSelection');
+const loadingEl = document.getElementById('loading');
+const houseSelectionEl = document.getElementById('houseSelection');
+const paperComponent = '<paper-component></paper-component>';
+const scissorsComponent = '<scissors-component></scissors-component>';
+const rockComponent = '<rock-component></rock-component>';
 let playerSelection = '';
 let playerComponent = '';
 
 paperEl.addEventListener('click', () => {
   playerSelection = 'paper';
-  playerComponent = '<paper-component></paper-component>';
+  playerComponent = paperComponent;
   nextPage();
 });
 
 scissorsEl.addEventListener('click', () => {
   playerSelection = 'scissors';
-  playerComponent = '<scissors-component></scissors-component>';
+  playerComponent = scissorsComponent;
   nextPage();
 });
 
 rockEl.addEventListener('click', () => {
   playerSelection = 'rock';
-  playerComponent = '<rock-component></rock-component>';
+  playerComponent = rockComponent;
   nextPage();
 });
 
@@ -32,4 +37,22 @@ function nextPage() {
   resultEl.classList.add('hidden');
 
   playerSelectionEl.innerHTML = playerComponent;
+
+  setTimeout(finalPage, 1000);
+}
+
+function finalPage() {
+  loadingEl.classList.add('hidden');
+
+  getHouseSelection();
+}
+
+function getHouseSelection() {
+  const arr = [paperComponent, scissorsComponent, rockComponent];
+
+  let random = Math.random();
+  random *= arr.length;
+  const index = Math.floor(random);
+
+  houseSelectionEl.innerHTML = arr[index];
 }
